7a775c78cce40cb5f41a235fe2f7c87656dad3ae
M0hamed-ehab <154088703+momoehab@users.noreply.github.com>
2025-10-04 14:25:50 +0300

    hashmap part 1

--- src/x_backend/src/lib.rs
+++ src/x_backend/src/lib.rs
@@ -1,83 +1,81 @@
 use candid::{CandidType, Deserialize};
 use ic_cdk::query;
 use ic_cdk::update;
+use std::cell::RefCell;
+use std::collections::HashMap;
 
-#[derive(Clone, CandidType, Deserialize)]
-struct List {
-    user: String,
-    items: Vec<Item>,
-}
 #[derive(Clone, CandidType, Deserialize)]
 struct Item {
     id: usize,
     data: String,
     state: bool,
 }
-
-static mut melist: List = List {
-    user: String::new(),
-    items: Vec::new(),
-};
+thread_local! {
+static melist: RefCell<HashMap<String, Vec<Item>>> = RefCell::new(HashMap::new());
+}
+//static mut melist: HashMap<String, Vec<Item>> = HashMap::new();
 
 #[query]
 fn greet(mut name: String) -> String {
     name = name.trim().to_string();
     unsafe {
-        if melist.user != name {
-            melist = List {
-                user: name.clone(),
-                items: Vec::new(),
-            };
-        } else {
-            melist.user = name.clone();
+        if !melist.contains_key(&name) {
+            melist.insert(name.clone(), Vec::new());
         }
     }
     format!("Hello, {}!", name)
 }
 #[query]
-fn showlist() -> Vec<Item> {
-    unsafe { melist.items.clone() }
+fn showlist(name: String) -> Vec<Item> {
+    unsafe { melist.get(&name).cloned().unwrap_or_default() }
 }
 
 #[update]
-fn changestate(num: usize) {
+fn changestate(name: String, num: usize) {
     unsafe {
-        if let Some(item) = melist.items.iter_mut().find(|i| i.id == num) {
-            item.state = !item.state;
+        if let Some(x) = melist.get_mut(&name) {
+            if let Some(item) = x.iter_mut().find(|i| i.id == num) {
+                item.state = !item.state;
+            }
         }
     }
 }
 
 #[update]
-fn addtolist(word: String) {
+fn addtolist(name: String, word: String) {
     unsafe {
-        melist.items.push(Item {
-            id: melist.items.len() + 1,
-            data: word,
-            state: false,
-        });
+        if let Some(x) = melist.get_mut(&name) {
+            x.push(Item {
+                id: x.len() + 1,
+                data: word,
+                state: false,
+            });
+        }
     }
 }
 
 #[update]
-fn removetolist(num: usize) {
+fn removetolist(name: String, num: usize) {
     unsafe {
-        if let Some(pos) = melist.items.iter().position(|i| i.id == num) {
-            melist.items.remove(pos);
+        if let Some(x) = melist.get_mut(&name) {
+            if let Some(pos) = x.iter().position(|i| i.id == num) {
+                x.remove(pos);
+            }
         }
     }
 }
 #[update]
-fn removeall() {
+fn removeall(name: String) {
     unsafe {
-        melist.items.clear();
+        if let Some(x) = melist.get_mut(&name) {
+            x.clear();
+        }
     }
 }
 #[update]
-fn deleteuser() {
+fn deleteuser(name: String) {
     unsafe {
-        melist.user = "".to_string();
-        melist.items.clear();
+        melist.remove(&name);
     }
 }
 
